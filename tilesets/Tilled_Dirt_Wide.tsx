f80c5538b18a1ac603ad22eb6a328b7418ff4880
rexchen2007 <rexchen2007@gmail.com>
2025-10-23 00:53:22 +0800

    Delete tilesets directory

--- tilesets/Tilled_Dirt_Wide.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Tilled_Dirt_Wide" tilewidth="32" tileheight="32" tilecount="15" columns="5">
- <image source="Tilled_Dirt_Wide.png" width="176" height="112"/>
-</tileset>
