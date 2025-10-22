65a7eee431423c65ab9086d84dc188583556f655
rexchen2007 <rexchen2007@gmail.com>
2025-10-22 13:25:40 +0800

    Add files via upload

---
+++ tilesets/assets_basic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="assets_basic" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../../../simple_tileset_32x32_version1.0/simple_tileset_32x32_version1.0/assets_basic.png" width="128" height="128"/>
+</tileset>
