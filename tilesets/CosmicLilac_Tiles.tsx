11ba38451d060491d69036f3cd769e0e4cb66883
rexchen2007 <rexchen2007@gmail.com>
2025-10-29 19:31:26 +0800

    Delete tilesets directory

--- tilesets/CosmicLilac_Tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="CosmicLilac_Tiles" tilewidth="32" tileheight="32" tilecount="24" columns="6">
- <image source="CosmicLilac_Tiles.png" width="208" height="128"/>
-</tileset>
