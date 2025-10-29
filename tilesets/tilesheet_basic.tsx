ffa32deffcce5cf16d36598637343deb0fefb6bc
rexchen2007 <rexchen2007@gmail.com>
2025-10-29 20:21:34 +0800

    Add files via upload

---
+++ tilesets/tilesheet_basic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilesheet_basic" tilewidth="32" tileheight="32" tilecount="160" columns="16">
+ <image source="../../../simple_tileset_32x32_version1.0/simple_tileset_32x32_version1.0/tilesheet_basic.png" width="512" height="320"/>
+</tileset>
